13b74a836f240bdbff84608f1cdcf1ddb6066ac8
Ben Tea <9259833+blueberryapple@users.noreply.github.com>
2017-09-26 16:23:11 -0700

    basic model of dday clone

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
+// standard
 import React from 'react';
-import { StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
-import { Madoka } from 'react-native-textinput-effects';
-import { Font } from 'expo';
+import {
+    StyleSheet,
+    Text,
+    KeyboardAvoidingView,
+    View
+} from 'react-native';
 
-const borderColor = '#67eaa2';
-const inputStyle = { color: borderColor };
+// additional
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Font } from 'expo';
+import {
+    Madoka,
+    Makiko
+} from 'react-native-textinput-effects';
+
+// constants
+const lime = '#67eaa2';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { fontLoaded: false };
+        this.state = {
+            fontLoaded: false,
+            price: 0,
+            rate: 1
+        };
     }
 
     async componentDidMount() {
@@ -22,13 +38,35 @@ export default class App extends React.Component {
     }
 
     render() {
+        let { price, rate } = this.state;
+        let daysLeft = Math.ceil(price / rate);
+
+        const sharedStyling = {
+            borderColor: lime,
+            keyboardType: 'numeric',
+            inputStyle: {
+                color: lime
+            },
+        }
+
         return (
             <KeyboardAvoidingView behavior={'padding'} style={styles.container}>
+                <Text style={styles.text}>D-Day Kdrama Clone</Text>
+                <Text style={styles.result}>{daysLeft} Days</Text>
+                <Text style={styles.body}>Enter the price and your daily earnings.</Text>
+
                 {this.state.fontLoaded &&
-                    <Madoka
-                        label={'Madoka'}
-                        borderColor={borderColor}
-                        inputStyle={inputStyle} />
+                    <View>
+                        <Madoka
+                            {...sharedStyling}
+                            label={'Price'}
+                            onChangeText={(price) => this.setState({ price })} />
+
+                        <Madoka
+                            {...sharedStyling}
+                            label={'Rate'}
+                            onChangeText={(rate) => this.setState({ rate })} />
+                    </View>
                 }
             </KeyboardAvoidingView>
        );
@@ -42,4 +80,16 @@ const styles = StyleSheet.create({
         padding: 40,
         backgroundColor: 'black',
     },
+    text: {
+        fontSize: 24,
+        color: lime
+    },
+    body: {
+        fontSize: 12,
+        color: lime
+    },
+    result: {
+        fontSize: 40,
+        color: 'white',
+    }
 });
